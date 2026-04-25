ebccedbeb0596a4528b15e92d4f08bf84df6951d
TharaniDharan-R <dharanidharan0404@gmail.com>
2026-04-25 20:42:57 +0530

    Time: 3 ms (31.97%), Space: 43.1 MB (60.97%) - LeetHub

---
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int s=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+               int l=j-i+1;
+                if(l%2!=0){
+                    for(int k=i;k<=j;k++){
+                        //System.out.print(arr[k]);
+                        //q=a[i]
+                        s+=arr[k];
+                    }
+                }
+                    //System.out.println();
+            }
+        }
+        return s;
+    }
+}

f733620813345945d542a2d80b92e2ac12c0e3fe
THARANI DHARAN R <dharanidharan0404@gmail.com>
2026-06-05 14:46:09 +0530

    Time: 4 ms (2.19%), Space: 43.1 MB (36.25%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int n=gain.length;
+        int a[]=new int[n+1];
+        a[0]=0;
+        int p=1;
+        for(int i=0;i<n;i++){
+            a[p]=a[p-1]+gain[i];
+            p++;
+        }
+        Arrays.sort(a);
+        
+        return a[n];
+    }
+}

16f4670c4022846359816796e17823f40d3a09e5
THARANI DHARAN R <dharanidharan0404@gmail.com>
2026-06-07 20:07:56 +0530

    Time: 2 ms (11.11%), Space: 47.9 MB (8.52%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        ArrayList<Integer> total= new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(i==0){
+                total.add(nums[i]);
+            }
+            else
+            total.add(nums[i]+total.get(i-1));
+        }
+        int ts= total.get(total.size()-1);
+        int ls =0;
+        for(int i=0;i<nums.length;i++){
+            if(i==0){
+                ls=0;
+            }
+            else{
+            ls =total.get(i-1);
+            }
+            int rs= ts - total.get(i);
+            if(ls==rs){
+                return i;
+            }
+        }
+       return -1;
+    }
+}

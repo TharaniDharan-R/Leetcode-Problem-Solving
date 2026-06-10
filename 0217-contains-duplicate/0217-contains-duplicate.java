3a4b3ac729b31acc7bd60cde044e7c38622cffd9
THARANI DHARAN R <dharanidharan0404@gmail.com>
2026-06-10 14:29:37 +0530

    Time: 21 ms (30.45%), Space: 80.3 MB (81.37%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -7,22 +7,22 @@ class Solution {
         //      return true;
         // }
         // // return false;
-        // HashSet<Integer> set=new HashSet<>();
-        // for(int i:nums){
-        //     set.add(i);
-        // } 
-        // //System.out.print(set);
-        // if(set.size()==nums.length)
-        // return false;
-        // return true;
-        Map<Integer,Integer> hm=new HashMap<>();
+        HashSet<Integer> set=new HashSet<>();
         for(int i:nums){
-            hm.put(i, hm.getOrDefault(i,0)+1);
-        }
-        for(int i:nums){
-            if(hm.get(i)>1)
-            return true;
-        }
+            set.add(i);
+        } 
+        //System.out.print(set);
+        if(set.size()==nums.length)
         return false;
+        return true;
+        // Map<Integer,Integer> hm=new HashMap<>();
+        // for(int i:nums){
+        //     hm.put(i, hm.getOrDefault(i,0)+1);
+        // }
+        // for(int i:nums){
+        //     if(hm.get(i)>1)
+        //     return true;
+        // }
+        // return false;
     }
 }

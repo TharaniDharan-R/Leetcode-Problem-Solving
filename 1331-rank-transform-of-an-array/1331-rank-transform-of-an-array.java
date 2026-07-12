b4876436f96d8428a7630d4201a0ec36660ec7f9
THARANI DHARAN R <dharanidharan0404@gmail.com>
2026-07-12 20:05:43 +0530

    Time: 101 ms (5.22%), Space: 82 MB (5.01%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int ans [] = new int[arr.length];
+        int ans1 [] = new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            ans1[i]= arr[i];
+            
+        }
+        Arrays.sort(ans1);
+        TreeSet <Integer> ts=new TreeSet<>();
+
+
+        TreeMap<Integer,Integer>hm= new TreeMap<>();
+        for(int i=0;i<ans1.length;i++){
+            ts.add(ans1[i]);
+        }
+        int c=1;
+        for(int i:ts){
+            
+            hm.put(i,c++);
+        }
+        for(int j=0;j<arr.length;j++){
+            if(hm.containsKey(arr[j])){
+                ans[j]=hm.get(arr[j]);
+            }
+        }
+        return ans;
+    }
+}

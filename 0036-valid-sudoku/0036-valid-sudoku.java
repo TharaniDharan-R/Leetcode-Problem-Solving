22f92380afcce746676991524b909bc39dec872f
THARANI DHARAN R <dharanidharan0404@gmail.com>
2026-09-07 15:01:23 +0530

    Time: 12 ms (15.25%), Space: 52.3 MB (11.88%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,17 +1,16 @@
-
 class Solution {
-    public boolean isValidSudoku(char[][] b) {
-        Set<String> s= new HashSet<>();
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                char c=b[i][j];
-                if(c!='.'){
-                    if(!s.add(c+"r"+i)||!s.add(c+"c"+j)||!s.add(c+"b"+i/3+j/3)){
-                        return false;
-                    }
+    public boolean isValidSudoku(char[][] board) {
+    Set s= new HashSet<>();
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            char c= board[i][j];
+            if(c!='.'){
+                if(!s.add(c+"c"+i)||!s.add(c+"r"+j)||!s.add(c+"b"+i/3+j/3)){
+                    return false;
                 }
             }
         }
-        return true;
+    }
+    return true;
     }
 }

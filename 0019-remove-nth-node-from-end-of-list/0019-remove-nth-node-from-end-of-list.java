58e6d338fbd1a2877cc76897350372a321c54e0a
THARANI DHARAN R <dharanidharan0404@gmail.com>
2026-09-15 12:34:07 +0530

    Time: 0 ms (100%), Space: 43.5 MB (39.8%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -7,8 +7,8 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
-class Solution {
+
+ class Solution {
     public static ListNode Reverse(ListNode head){
         ListNode prev= null;
         ListNode curr=head;
@@ -38,5 +38,25 @@ class Solution {
         }
         head=Reverse(head);
         return head;
+    }
+}
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode slow=dummy;
+        ListNode fast=dummy;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+        while(fast.next!= null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        if(fast.next==null){
+            slow.next=slow.next.next;
+        }
+        return dummy.next;
     }
 }

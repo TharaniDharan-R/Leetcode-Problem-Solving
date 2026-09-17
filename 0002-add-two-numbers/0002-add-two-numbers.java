b753663ec112b9835877a4ba07dd6a21bdcc8d1d
THARANI DHARAN R <dharanidharan0404@gmail.com>
2026-09-17 12:11:14 +0530

    Time: 1 ms (100%), Space: 46.5 MB (57.66%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,32 +10,68 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode ans =new ListNode();
-        ListNode t1 = l1;
-        ListNode t2 = l2;
-        int sum =0;
-        int c=0;
+        ListNode ans= new ListNode();
+        ListNode t1=l1;
+        ListNode t2=l2;
         ListNode res= ans;
-        while(t1 != null || t2 != null){
-            sum= c;
-            if(t1 !=null){
-                sum +=t1.val;
-            t1=t1.next;
+        int sum=0;
+        int c=0;
+        while(t1!=null || t2 != null){
+            sum=c;
+            if(t1!=null){
+                sum+=t1.val;
+                t1=t1.next;
             }
-            if(t2 != null){
+            if(t2!=null){
                 sum+=t2.val;
-            t2= t2.next;
+                t2=t2.next;
             }
             ListNode n=new ListNode(sum%10);
             c=sum/10;
             res.next=n;
-            res = res.next;
+            res=res.next;
         }
         if(c>0){
-            ListNode a= new ListNode(c);
-            res.next= a;
-            res= res.next;
+            ListNode last= new ListNode(c%10);
+            res.next=last;
+            res=res.next;
         }
         return ans.next;
+
+
+
+
+
+
+
+
+
+        // ListNode ans =new ListNode();
+        // ListNode t1 = l1;
+        // ListNode t2 = l2;
+        // int sum =0;
+        // int c=0;
+        // ListNode res= ans;
+        // while(t1 != null || t2 != null){
+        //     sum= c;
+        //     if(t1 !=null){
+        //         sum +=t1.val;
+        //     t1=t1.next;
+        //     }
+        //     if(t2 != null){
+        //         sum+=t2.val;
+        //     t2= t2.next;
+        //     }
+        //     ListNode n=new ListNode(sum%10);
+        //     c=sum/10;
+        //     res.next=n;
+        //     res = res.next;
+        // }
+        // if(c>0){
+        //     ListNode a= new ListNode(c);
+        //     res.next= a;
+        //     res= res.next;
+        // }
+        // return ans.next;
     }
 }
